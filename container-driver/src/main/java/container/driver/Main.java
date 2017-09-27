141cc19cf245ebef4cd13f16fc212acaedcde8b3
Brian Toal <brian.toal@gmail.com>
2017-09-26 23:38:10 -0700

    Added logback, still need to add it in a more modular way

--- container-driver/src/main/java/container/driver/Main.java
+++ container-driver/src/main/java/container/driver/Main.java
@@ -1,11 +1,17 @@
 package container.driver;
 
 import container.webapp.api.WebContainer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	
 	public static void main(String[] args) {
+		logger.info("Starting " + Main.class.getSimpleName());
+
 		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext();
 		acac.scan("configuration");
 		acac.refresh();
