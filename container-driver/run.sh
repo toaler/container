cf7ff8a53233b7a5539739d13e63473189748b5d
Brian <btoal@salesforce.com>
2018-08-23 11:01:36 -0700

    Added startup background/foreground modes

--- container-driver/run.sh
+++ container-driver/run.sh
@@ -3,17 +3,22 @@
 # run.sh <war>
 JAVA_HOME=/Users/btoal/blt/tools/Darwin/jdk/jdk1.8.0_66_x64
 DRIVER_HOME=/Users/btoal/git/container/container-driver
-LOG=`date +%Y%m%d%H%M%S`.log
 #TODO GRAB PIDFILE NAME FROM $1
 PIDFILE=/tmp/container-example-webapp-0.0.1-SNAPSHOT.pid
+CMD="${JAVA_HOME}/bin/java -Dwc.context.path=/ -Xbootclasspath/p:${DRIVER_HOME}/lib/alpn-boot-8.1.11.v20170118.jar -cp ${DRIVER_HOME}/target/container-driver-0.0.1-SNAPSHOT.jar:${DRIVER_HOME}/lib/* container.driver.Main"
 
 start() {
   if pgrep -F ${PIDFILE}
   then
     echo "Application already running"
   else
-    nohup ${JAVA_HOME}/bin/java -Dwc.context.path=/ -Xbootclasspath/p:${DRIVER_HOME}/lib/alpn-boot-8.1.11.v20170118.jar -cp ${DRIVER_HOME}/target/container-driver-0.0.1-SNAPSHOT.jar:${DRIVER_HOME}/lib/* container.driver.Main $1 | tee ${LOG} &
-    echo "Started application"
+    if [ "$2" = true ]
+    then
+      nohup $CMD $1 &
+      echo "Started application in background, see status to validate state and log to troubleshoot"
+    else
+      $CMD $1
+    fi
   fi 
 }
 
@@ -28,8 +33,11 @@ status() {
 
 ### main logic ###
 case "$1" in
+startfg)
+  start $2 false
+  ;;
 start)
-  start $2
+  start $2 true
   ;;
 stop)
   stop
