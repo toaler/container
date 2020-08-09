242069504eabe808bfa331aa72f6bf4ccc6b099e
toaler <brian.toal@gmail.com>
2020-08-09 00:51:20 -0700

    Cursory version of ETags

--- container-example-webapp/src/main/java/configuration/AppConfig.java
+++ container-example-webapp/src/main/java/configuration/AppConfig.java
@@ -1,5 +1,6 @@
 package configuration;
 
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,24 +11,37 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
 import component.HealthCheckServiceImpl;
 import component.MyComponent;
 import resource.Healthcheck;
 
 /**
- * Top level configuration responsible for wiring all Spring managed beans together.
+ * Top level configuration responsible for wiring all Spring managed beans
+ * together.
  * 
  * @author toal
  *
  */
 @Configuration
-@Import({MyComponent.class, Healthcheck.class, HealthCheckServiceImpl.class})
+@Import({ MyComponent.class, Healthcheck.class, HealthCheckServiceImpl.class })
 public class AppConfig {
 
-    @Lazy
-    @Bean("logger")
-    @Scope("prototype")
-    public Logger getLogger(InjectionPoint injectionPoint) {
-        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
-    }
+	@Lazy
+	@Bean("logger")
+	@Scope("prototype")
+	public Logger getLogger(InjectionPoint injectionPoint) {
+		return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
+	}
+
+	@Lazy
+	@Bean("eTagCache")
+	public Cache<String, String> getETagCache() {
+		Cache<String, String> eTagCache = Caffeine.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS)
+				.maximumSize(10_000).build();
+
+		return eTagCache;
+	}
 }
--- container-example-webapp/src/main/java/servlet/ApplicationContextAccessServlet.java
+++ container-example-webapp/src/main/java/servlet/ApplicationContextAccessServlet.java
@@ -1,10 +1,9 @@
 package servlet;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
-import java.util.TimeZone;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -15,33 +14,92 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import com.github.benmanes.caffeine.cache.Cache;
+
 import component.MyComponent;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = { "/applicationcontext" })
 public class ApplicationContextAccessServlet extends HttpServlet {
-	
+
 	@Autowired
 	private Logger logger;
-	
+
 	@Autowired
 	private MyComponent myComponent;
 
+	@Autowired
+	private Cache<String, String> eTagCache;
+
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
-				config.getServletContext());
+		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, config.getServletContext());
 	}
-	
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		logger.info("doGet");
-		response.setContentType("text/html");
-		response.setHeader("cache-control","private, max-age=600");
-		
-		response.getWriter().write(new Date().toString() + "\n");
 
-		response.getWriter().write(myComponent.message() + "\n");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		logger.info("doGet");
+
+		var providedETag = request.getHeader("If-None-Match");
+
+		if (providedETag != null) {
+
+			String cachedResponse = eTagCache.getIfPresent(providedETag);
+
+			if (cachedResponse != null) {
+				response.setStatus(304);
+				return;
+			}
+
+		}
+
+		response.setContentType("text/html");
+		response.setHeader("cache-control", "private, max-age=5");
+
+		var sb = new StringBuilder();
+		sb.append(new Date().toString() + "\n");
+		sb.append(myComponent.message() + "\n");
+
+		var responseText = sb.toString();
+		String eTag = generateEtag(responseText);
+
+		eTagCache.put(eTag, responseText);
+
+		response.setHeader("ETag", eTag);
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		response.getWriter().write(responseText);
+	}
+
+	public String generateEtag(String input) {
+		var eTag = "";
+		try {
+			// Create MessageDigest instance for MD5
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			// Add password bytes to digest
+			md.update(input.getBytes());
+			// Get the hash's bytes
+			byte[] bytes = md.digest();
+			// This bytes[] has bytes in decimal format;
+			// Convert it to hexadecimal format
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			// Get complete hashed password in hex format
+			eTag = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		return eTag;
 	}
 
 }
